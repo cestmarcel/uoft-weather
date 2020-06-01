a0b7412ad6201d07a8fa6042dc1a6d83c8105845
cestmarcel <25566817+cestmarcel@users.noreply.github.com>
2020-05-31 20:24:18 -0400

    Implemented rendering of searched cities and weather details

---
+++ js/script.js
@@ -0,0 +1,115 @@
+var cityQuery;
+var cityLat;
+var cityLong;
+var weather;
+var cityHistory = [];
+
+async function fetchWeather(){
+    weather = await fetch( `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLong}&exclude=hourly&appid=df7ce556761f98dad07fc817248b0429` ).then( (weather)=>weather.json() )
+    console.log(weather);
+    renderWeather();
+    if(localStorage.getItem("cityHistory")){
+        cityHistory = JSON.parse(localStorage.getItem("cityHistory"));
+    }
+    if(!cityHistory.find(function(cities){
+        return cities.city == cityQuery;
+    })){
+        cityHistory.push({city: cityQuery, lat: cityLat, long: cityLong});
+        localStorage.setItem("cityHistory", JSON.stringify(cityHistory));
+    }
+}
+
+async function fetchLocation(){
+    const result = await fetch( `https://api.opencagedata.com/geocode/v1/json?q=${cityQuery}&key=15c163f7b80843da8be9c263a4aad238` ).then( (result)=>result.json() )
+    cityLat = result.results[0].geometry.lat;
+    cityLong = result.results[0].geometry.lng;
+    fetchWeather();
+}
+
+function renderWeather(){
+    document.querySelector("#city-input").value = "";
+    document.querySelector(".forecast-div").style = "display: block";
+    document.querySelector("#content-city").textContent = cityQuery;
+    document.querySelector("#temperature").textContent = `Temperature: ${(weather.current.temp-273.15).toFixed(1)} °C`;
+    document.querySelector("#humidity").textContent = `Humidity: ${weather.current.humidity} %`;
+    document.querySelector("#wind").textContent = `Wind speed: ${weather.current.wind_speed} km/h`;
+    document.querySelector("#uv").innerHTML = `UV index: <span class="uv-warning">${weather.current.uvi}</span>`;
+    document.querySelector(".header-icon").src = `http://openweathermap.org/img/wn/${weather.current.weather[0].icon}@2x.png`
+    var uvi = weather.current.uvi;
+    if(uvi<=2){
+        document.querySelector(".uv-warning").style = "background-color: green; color: white";
+    } else if(uvi>2 && uvi<=5){
+        document.querySelector(".uv-warning").style = "background-color: yellow";
+    } else if(uvi>5 && uvi<=7){
+        document.querySelector(".uv-warning").style = "background-color: orange";
+    } else if(uvi>7 && uvi<=10){
+        document.querySelector(".uv-warning").style = "background-color: red; color: white";
+    } else if(uvi>10){
+        document.querySelector(".uv-warning").style = "background-color: violet";
+    };
+    document.querySelector("#today").textContent = `Today, ${moment().format("LL")}`;
+    for(i=0; i<5; i++){
+        document.querySelector(`#forecast-${i}-day`).textContent = moment().add(i, 'day').format("LL");
+        document.querySelector(`#forecast-${i}-icon`).src = `http://openweathermap.org/img/wn/${weather.daily[i].weather[0].icon}@2x.png`;
+        document.querySelector(`#forecast-${i}-temp`).textContent = `Temp: ${(weather.daily[i].temp.day-273.15).toFixed(1)} °C`;
+        document.querySelector(`#forecast-${i}-humid`).textContent = `Humidity: ${weather.daily[i].humidity} %`;
+    }
+}
+
+// Render buttons for each searched city
+document.querySelector("#city-search").addEventListener("click", function(){
+    cityQuery = document.querySelector("#city-input").value;
+    if(!cityHistory.find(function(cities){
+        return cities.city == cityQuery;
+    })){
+        document.querySelector("#search-history").innerHTML += `<button type="button" class="btn btn-outline-dark">${cityQuery}</button>`
+        fetchLocation();
+    } else {
+        document.querySelector("#city-input").value = "";
+    }
+})
+
+// Make searched cities clickable
+const buttons = document.querySelector("#search-history");
+buttons.addEventListener("click", function(event){
+    if(event.target.className == "btn btn-outline-dark"){
+        cityQuery = event.target.textContent;
+        fetchLocation();
+    }
+})
+
+// Render search history upon loading the page
+function renderHistory(){
+    if(localStorage.getItem("cityHistory")){
+        cityHistory = JSON.parse(localStorage.getItem("cityHistory"));
+        cityHistory.forEach(function(data){
+            document.querySelector("#search-history").innerHTML += `<button type="button" class="btn btn-outline-dark">${data.city}</button>`;
+        })
+        cityQuery = cityHistory[0].city;
+        document.querySelector(".forecast-div").style = "display: block";
+        fetchLocation();
+    }
+}
+renderHistory();
+
+// Clear history and reset overview
+function clearHistory(){
+    if(localStorage.getItem("cityHistory")){
+        localStorage.removeItem("cityHistory");
+        cityQuery = "";
+        cityLat = "";
+        cityLong = "";
+        weather = "";
+        cityHistory = [];
+        document.querySelector("#search-history").innerHTML = "";
+        document.querySelector(".forecast-div").style = "display: none";
+        document.querySelector("#content-city").textContent = "Welcome to your weather forecast!";
+        document.querySelector("#temperature").textContent = "";
+        document.querySelector("#humidity").textContent = "";
+        document.querySelector("#wind").textContent = "";
+        document.querySelector("#uv").textContent = "";
+        document.querySelector(".header-icon").src = "";
+        document.querySelector(".uv-warning").style = "background-color: white";
+        document.querySelector("#today").textContent = "";
+    }
+}
